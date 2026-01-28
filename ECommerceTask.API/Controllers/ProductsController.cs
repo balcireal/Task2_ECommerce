12178dd85bc5ba8e3cecd2f3d49341aa8dc60214
Adil Can Balcı <balciadilcan@gmail.com>
2026-01-28 21:19:33 +0300

    Faz 1 tamamlandi: Product CRUD, CQRS ve DB baglantisi basarili

---
+++ ECommerceTask.API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using ECommerceTask.Application.Features.Products.Commands.CreateProduct;
+using ECommerceTask.Application.Features.Products.Queries.GetAllProducts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceTask.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllProductsQuery();
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            var productId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetAll), new { id = productId }, command);
+        }
+    }
+}
